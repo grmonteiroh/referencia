33a7fa7057a4620dba3af5846359faea1ac3507a
Gustavo Rodrigues Monteiro <grmonteiro@ymail.com>
2025-02-05 23:39:48 -0300

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -13,3 +13,20 @@ SELECT conta, data, saldo
 FROM saldos_filtrados
 WHERE data <= primeira_vez_zero OR primeira_vez_zero IS NULL
 ORDER BY conta, data;
+
+---
+WITH saldos_filtrados AS (
+    SELECT 
+        conta, 
+        data, 
+        saldo, 
+        MIN(data) FILTER (WHERE saldo = 0) 
+            OVER (PARTITION BY conta) AS primeira_vez_zero
+    FROM transacoes
+    WHERE data BETWEEN DATE '@start_date' AND DATE '@end_date'
+      AND conta IN ('@lista_contas')
+)
+SELECT conta, data, saldo
+FROM saldos_filtrados
+WHERE data <= primeira_vez_zero OR primeira_vez_zero IS NULL
+ORDER BY conta, data;
