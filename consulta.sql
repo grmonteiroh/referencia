f4a9c3451124ccc6c8e70860afc1ad9b975271cb
Gustavo Rodrigues Monteiro <grmonteiro@ymail.com>
2025-02-05 23:35:54 -0300

    Create consulta.sql

---
+++ consulta.sql
@@ -0,0 +1,15 @@
+WITH saldos_filtrados AS (
+    SELECT 
+        conta, 
+        data, 
+        saldo, 
+        MIN(CASE WHEN saldo = 0 THEN data END) 
+            OVER (PARTITION BY conta) AS primeira_vez_zero
+    FROM transacoes
+    WHERE data BETWEEN '@start_date' AND '@end_date'
+      AND conta IN ('@lista_contas')
+)
+SELECT conta, data, saldo
+FROM saldos_filtrados
+WHERE data <= primeira_vez_zero OR primeira_vez_zero IS NULL
+ORDER BY conta, data;
